2eb9969adc16382a918812e628a400cc8b4be09e
macbre <maciej.brencz@gmail.com>
2019-02-04 18:56:54 +0100

    Add some test fixtures

---
+++ Makefile
@@ -0,0 +1,21 @@
+coverage_options = --omit='test/*'
+
+install:
+	pip install -e .[dev]
+
+test:
+	pytest -vv
+
+coverage:
+	rm -f .coverage*
+	rm -rf htmlcov/*
+	coverage run -p -m pytest -vv
+	coverage combine
+	coverage html -d htmlcov $(coverage_options)
+	coverage xml -i
+	coverage report $(coverage_options)
+
+lint:
+	pylint now_playing_graph
+
+.PHONY: test
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+# @see https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# @see https://github.com/pypa/sampleproject/blob/master/setup.py
+setup(
+    name='now_playing_graph',
+    version=VERSION,
+    author='Maciej Brencz',
+    author_email='maciej.brencz@gmail.com',
+    license='MIT',
+    description='Processes "now playing" data from internet radio player for Faroese Útvarp',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/macbre/now-playing-graph',
+    packages=find_packages(),
+    extras_require={
+        'dev': [
+            'coverage==4.5.1',
+            'pylint==2.1.1',
+            'pytest==3.9.3',
+        ]
+    },
+    install_requires=[
+    ]
+)
